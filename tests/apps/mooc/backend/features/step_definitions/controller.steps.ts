43563169509087c0e2595379a1185df24918866d
Jose Ugal <joseugal17@icloud.com>
2022-12-13 22:24:08 +0100

    chore: :memo: First endpoint, dependency injector, integration tests, etc...

--- tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import { AfterAll, BeforeAll, Given, Then } from 'cucumber';
 import request from 'supertest';
 import { MoocBackendApp } from '../../../../../../src/apps/mooc/backend/MoocBackendApp';
@@ -13,14 +12,6 @@ Given('I send a GET request to {string}', (route: string) => {
 
 Then('the response status code should be {int}', async (status: number) => {
   _response = await _request.expect(status);
-});
-
-Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
-  _request = request(application.httpServer).put(route).send(JSON.parse(body));
-});
-
-Then('the response should be empty', () => {
-  assert.deepStrictEqual(_response.body, {});
 });
 
 BeforeAll(async () => {
